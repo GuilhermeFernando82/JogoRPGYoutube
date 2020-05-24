62d228ef082bf51aaeb6b41f0a7717d8b76d77f0
Guilherme Fernando da Silva de Oliveira <guilherme.oliveira@gebit.io>
2020-05-23 22:17:03 -0300

    Edit

---
+++ Enemy.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private float vel = 1f;
+    private Transform alvo;
+    private Vector3 startPos;
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        perseguir();
+    }
+    private void OnTriggerEnter2D(Collider2D collision){
+        if(collision.CompareTag("Player")){
+            alvo = collision.transform;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision){
+        if(collision.CompareTag("Player")){
+            alvo = null;
+        }
+    }
+    void perseguir(){
+        if(alvo != null){
+            transform.position = Vector2.MoveTowards(transform.position, alvo.position, vel * Time.deltaTime);
+            float dist = Vector2.Distance(transform.position, alvo.position);
+            if(dist <= 0.8f){
+                vel = 0;
+            }else{
+                vel = 1.5f;
+            }
+        }else{
+            transform.position = Vector2.MoveTowards(transform.position, startPos, vel * Time.deltaTime);
+        }
+
+
+    }
+}
---
+++ camera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+   private Vector2 velocity;
+ 
+   public float smoothTimeY;
+   public float smoothTimeX;
+   public float deltaY;
+ 
+   public GameObject player;
+ 
+   void Start(){
+      player = GameObject.FindGameObjectWithTag("Player");
+      smoothTimeY = 0.5f;
+      smoothTimeX = 0.5f;
+      deltaY = 0.5f;
+   }
+ 
+   void FixedUpdate(){
+      float posX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x, ref velocity.x, smoothTimeX);
+      float posY = Mathf.SmoothDamp(transform.position.y, player.transform.position.y + deltaY, ref velocity.y, smoothTimeY);
+      transform.position = new Vector3(posX, posY, transform.position.z);
+   }
+}
