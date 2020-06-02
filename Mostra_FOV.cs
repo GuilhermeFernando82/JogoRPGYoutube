95eb697be01733a36f42143a76309da2121ba1a6
Guilherme Fernando da Silva de Oliveira <guilherme.oliveira@gebit.io>
2020-06-02 14:22:22 -0300

    visaovilao

---
+++ Mostra_FOV.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor; 
+[CustomEditor(typeof(visaovilao))]
+public class Mostra_FOV : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public visaovilao fovM;
+
+   private void OnSceneGUI()
+   {
+       
+        Handles.color = Color.black;
+	    Handles.DrawWireArc(fovM.transform.position, Vector2.up, Vector2.up,360,fovM.fov);
+        Vector3 vA = -fovM.DirFAngle(fovM.fov / 2);
+        Vector3 vB = fovM.DirFAngle(fovM.fov / 2);
+        
+        Handles.DrawLine(fovM.transform.position, fovM.transform.position + vA * fovM.raioVisao);
+        Handles.DrawLine(fovM.transform.position, fovM.transform.position + vB * fovM.raioVisao);
+   }
+}
---
+++ visaovilao.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class visaovilao : MonoBehaviour
+{
+[SerializeField]
+    public float raioVisao,raioAtaque,vel;
+    public LayerMask oqePlayer;
+ 
+    [SerializeField]
+     private GameObject player;
+ 
+    [SerializeField]
+    private Rigidbody2D rb2D;
+ 
+    [SerializeField]
+    private Vector3 posInicial;
+    [SerializeField]
+    private Vector3 alvo;
+    [SerializeField]
+    public float fov;
+    [SerializeField]
+    private Animator anim;
+    [SerializeField]
+    private Vector3 dir;
+    
+    private void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        rb2D = GetComponent<Rigidbody2D>();
+        posInicial = transform.position;
+        alvo = posInicial;
+    }
+    public Vector3 DirFAngle(float angDeg){
+
+
+        return new Vector3(Mathf.Sin(angDeg * Mathf.Deg2Rad), Mathf.Cos(angDeg * Mathf.Deg2Rad),0);
+    }
+ 
+    private void Update()
+    {
+       /* if((Vector2.Angle(player.transform.position - transform.position, -transform.up)) <= fov * 0.5f){
+            RaioJogador();
+        }
+        */
+        if(dir.x != 0 || dir.y != 0)
+        {
+        anim.SetFloat("x", dir.x);
+        anim.SetFloat("y", dir.y);
+        }
+        RaioJogador();
+    }
+ 
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, raioVisao);
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, raioAtaque);
+    }
+ 
+    void RaioJogador()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, player.transform.position - transform.position, raioVisao, oqePlayer);
+        Vector3 temp = transform.TransformDirection(player.transform.position - transform.position);
+        Debug.DrawRay(transform.position, temp, Color.cyan);
+ 
+        if (hit.collider != null)
+        {
+            if (hit.collider.tag == "Player")
+            {
+                alvo = player.transform.position;
+            }
+           
+        }else
+        {
+            alvo = posInicial;
+        }
+        
+        float distanciaTemp = Vector3.Distance(alvo, transform.position);
+        dir = (alvo - transform.position).normalized;
+ 
+        if (alvo != posInicial && distanciaTemp < raioAtaque)
+        {
+            
+        }
+ 
+        else
+        {
+            rb2D.MovePosition(transform.position + dir * vel * Time.deltaTime);
+        }
+ 
+        if (alvo == posInicial && distanciaTemp <= 0.02f)
+        {
+            transform.position = posInicial;
+        }
+ 
+        Debug.DrawLine(transform.position, alvo, Color.green);
+    }
+}
